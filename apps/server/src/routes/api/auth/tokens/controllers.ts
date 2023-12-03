1d637edbaed6062cf155642a5773a1bbd4c95c56
danilupion <danilupion@gmail.com>
2023-12-03 23:35:25 +0100

    Improved unauthorized management

--- apps/server/src/routes/api/auth/tokens/controllers.ts
+++ apps/server/src/routes/api/auth/tokens/controllers.ts
@@ -6,9 +6,9 @@ import controller, {
 } from '@slangy/server/helpers/express/controller.js';
 import { ClientErrorUnauthorized } from '@slangy/server/helpers/httpError.js';
 import { SuccessStatusCode } from '@slangy/server/http.js';
-import { JwtData, generateToken } from '@slangy/server/middleware/express/auth/jwt.js';
+import { generateToken } from '@slangy/server/middleware/express/auth/jwt.js';
 
-import UserModel from '../../../../models/user.js';
+import UserModel, { UserDocument } from '../../../../models/user.js';
 import { getTokenPayload } from '../../../../utils/auth.js';
 
 export const createToken = controller<
@@ -31,16 +31,11 @@ export const createToken = controller<
   });
 });
 
-export const renewToken = controller<RequestWithFields<JwtData>, ResponseWithBody<CreateTokenRes>>(
-  async (req, res) => {
-    const user = await UserModel.findById(req.jwtUser.id);
-
-    if (!user) {
-      throw new ClientErrorUnauthorized();
-    }
-
-    return res
-      .status(SuccessStatusCode.SuccessOK)
-      .send({ token: await generateToken(getTokenPayload(user)) });
-  },
-);
+export const renewToken = controller<
+  RequestWithFields<{ user: UserDocument }>,
+  ResponseWithBody<CreateTokenRes>
+>(async (req, res) => {
+  return res
+    .status(SuccessStatusCode.SuccessOK)
+    .send({ token: await generateToken(getTokenPayload(req.user)) });
+});
--- apps/server/src/routes/api/auth/tokens/index.ts
+++ apps/server/src/routes/api/auth/tokens/index.ts
@@ -1,9 +1,11 @@
 import router from '@slangy/server/helpers/express/router.js';
 import jwt from '@slangy/server/middleware/express/auth/jwt.js';
 
+import { checkUser } from '../../middleware.js';
+
 import { createToken, renewToken } from './controllers.js';
 import { validateTokenCreation } from './validators.js';
 
 export default router()
   .post('/', validateTokenCreation, createToken) // POST: create a new token
-  .put('/', jwt(), renewToken); // PUT: renew a token
+  .put('/', jwt(), checkUser, renewToken); // PUT: renew a token

a603b453404c42e6f16dc5c6748efaeaa9ce17ec
danilupion <danilupion@gmail.com>
2023-12-09 13:57:25 +0100

    Reworked toasts life so that it is parametrized

--- apps/client/src/contexts/notifications/NotificationsProvider.tsx
+++ apps/client/src/contexts/notifications/NotificationsProvider.tsx
@@ -3,11 +3,15 @@ import { PropsWithChildren, useRef } from 'react';
 
 import NotificaitonsContext, { NotificationsContextType } from './NotificationsContext';
 
-const NotificationsProvider = ({ children }: PropsWithChildren) => {
+type NotificationsProviderProps = PropsWithChildren<{
+  delay?: number;
+}>;
+
+const NotificationsProvider = ({ children, delay = 5000 }: NotificationsProviderProps) => {
   const toastRef = useRef<Toast>(null);
 
   const showToast = (severity: ToastMessage['severity']) => (summary: string, detail: string) => {
-    toastRef.current?.show({ severity, summary, detail });
+    toastRef.current?.show({ severity, summary, detail, life: delay });
   };
 
   const severities = ['success', 'info', 'warn', 'error'] as const;

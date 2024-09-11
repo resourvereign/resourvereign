5e38fb63c3d011c0dbdda2a7d3c8ad462f3e6171
Dani Lupión <danilupion@gmail.com>
2024-09-11 23:43:41 +0200

    Fixed issue with build copy step

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,16 +7,17 @@ yarn build:libs;
 yarn build:apps;
 
 SERVER_PUBLIC="./apps/server/dist/public";
-CLIENT_BUILD="../../client/dist";
+CLIENT_BUILD_LINK_PATH="../../client/dist";
+CLIENT_BUILD_COPY_PATH="./apps/client/dist";
 
 [ -e "$SERVER_PUBLIC" ] && rm "$SERVER_PUBLIC"
 
 if [ "$ACTION" == "link" ]; then
-    echo "Linking $CLIENT_BUILD to $SERVER_PUBLIC"
-    ln -s "$CLIENT_BUILD" "$SERVER_PUBLIC"
+    echo "Linking $CLIENT_BUILD_LINK_PATH to $SERVER_PUBLIC"
+    ln -s "$CLIENT_BUILD_LINK_PATH" "$SERVER_PUBLIC"
 elif [ "$ACTION" == "copy" ]; then
-    echo "Copying $CLIENT_BUILD to $SERVER_PUBLIC"
-    cp -r "$CLIENT_BUILD" "$SERVER_PUBLIC"
+    echo "Copying $CLIENT_BUILD_COPY_PATH to $SERVER_PUBLIC"
+    cp -r "$CLIENT_BUILD_COPY_PATH" "$SERVER_PUBLIC"
 else
     echo "Please set the ACTION environment variable to either 'link' or 'copy'."
     exit 1

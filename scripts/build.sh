24e0e481aa4ba0ff4a9723598120ffd9286d0c1f
Dani Lupión <danilupion@gmail.com>
2024-09-11 23:38:10 +0200

    Conditionally link or copy fe depending on env variable

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,13 @@ CLIENT_BUILD="../../client/dist";
 
 [ -e "$SERVER_PUBLIC" ] && rm "$SERVER_PUBLIC"
 
-ln -s "$CLIENT_BUILD" "$SERVER_PUBLIC";
+if [ "$ACTION" == "link" ]; then
+    echo "Linking $CLIENT_BUILD to $SERVER_PUBLIC"
+    ln -s "$CLIENT_BUILD" "$SERVER_PUBLIC"
+elif [ "$ACTION" == "copy" ]; then
+    echo "Copying $CLIENT_BUILD to $SERVER_PUBLIC"
+    cp -r "$CLIENT_BUILD" "$SERVER_PUBLIC"
+else
+    echo "Please set the ACTION environment variable to either 'link' or 'copy'."
+    exit 1
+fi
